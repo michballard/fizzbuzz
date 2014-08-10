f5c60ee5330ef1d345fd81dba9443514b30493c0
michballard <michballard@gmail.com>
2014-08-10 21:35:05 +0100

    Adding fizzbuzz work

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,51 @@
+# This file is used to contain scenarios
+
+require 'fizzbuzz'
+
+describe 'fizzbuzz test' do
+
+	it 'knows that 3 is divisible by 3' do
+		expect(is_divisible_by_three?(3)).to be true
+	end
+
+	it 'knows that 4 is not divisible by 3' do 
+		expect(is_divisible_by_three?(4)).to be false
+	end
+
+	it 'knows that 5 is divisible by 5' do
+		expect(is_divisible_by_five?(5)).to be true
+	end
+
+	it 'knows that 6 is not divisible by 5' do
+		expect(is_divisible_by_five?(6)).to be false
+	end
+
+	it 'knows that 15 is divisible by 15' do
+		expect(is_divisible_by_fifteen?(15)).to be true
+	end
+
+	it 'knows that 16 is not divisible by 15' do
+		expect(is_divisible_by_fifteen?(16)).to be false
+	end
+
+	it 'knows that 1 will return 1' do
+		expect(fizzbuzz_response?(1)).to be 1
+	end
+
+	it "knows that 3 will return 'Fizz'" do
+		expect(fizzbuzz_response?(3)).to eq "Fizz"
+	end
+
+	it "knows that 5 will return 'Buzz'" do
+		expect(fizzbuzz_response?(5)).to eq "Buzz"
+	end
+
+	it "knows that 15 will return 'FizzBuzz'" do
+		expect(fizzbuzz_response?(15)).to eq "FizzBuzz"
+	end
+
+	it "plays the FizzBuzz game from 1 to 100" do
+
+	end
+
+end
